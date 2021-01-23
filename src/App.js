047f755d253a707e4efff983c6ab4d1761a328eb
Simon <29654835+absolutelysimon@users.noreply.github.com>
2021-01-23 13:46:53 -0700

    Routing changes.

--- src/App.js
+++ src/App.js
@@ -1,58 +1,47 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Header, Footer, Body } from "./Components";
-import Login from './Components/Pages/Login';
-import Events from './Components/Events';
-import Tutorial from './Components/Pages/Tutorial';
-import Home from './Components/Pages/Home';
-import Projects from './Components/Pages/Projects';
-import ProjectPage from './Components/Pages/ProjectPage';
+import Login from "./Components/Pages/Login";
+import Events from "./Components/Events";
+import Tutorial from "./Components/Pages/Tutorial";
+import Home from "./Components/Pages/Home";
+import Projects from "./Components/Pages/Projects";
+import ProjectPage from "./Components/Pages/ProjectPage";
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pages: [
-        "Home",
-        "Tutorial",
-        "Events",
-        "Projects",
-        "Join us",
-        "Contact"
-      ],
-      page: "Home"
+      pages: ["Home", "Tutorial", "Events", "Projects", "Join us", "Contact"],
+      page: "Home",
     };
   }
 
   handleClick(nextPage) {
     this.setState({
-      page: nextPage
+      page: nextPage,
     });
   }
 
   render() {
     return (
       <Router>
-          <Header
-            page={this.state.page}
-            onClick={i => this.handleClick(i)}
-            pages={this.state.pages}
-          />
-          <div style={{marginLeft: '10px', minHeight: 'calc(100vh - 110px)' }}>
-            <Switch>
-              <Route exact path="/home" component={Home} />
-              <Route exact path="/events" component={Events} />
+        <Header page={this.state.page} pages={this.state.pages} />
+        <div style={{ marginLeft: "10px", minHeight: "calc(100vh - 110px)" }}>
+          <Switch>
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/events" component={Events} />
 
-              <Route exact path="/tutorial" component={Tutorial} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/projects/" component={Projects} />
-              <Route exact path="/projects" component={Projects} />
-              <Route path={`/projects/:projectId`} component={ProjectPage} />
-            </Switch>
-          </div>
-          <Footer></Footer>
+            <Route exact path="/tutorial" component={Tutorial} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/projects/" component={Projects} />
+            <Route exact path="/projects" component={Projects} />
+            <Route path={`/projects/:projectId`} component={ProjectPage} />
+          </Switch>
+        </div>
+        <Footer></Footer>
       </Router>
     );
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,68 +1,85 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import logo from '../Logo3.1.png';
-import { useGlobal } from '../state';
-import 'antd/dist/antd.css';
-import Menu from 'antd/es/menu';
-import Button from 'antd/es/button';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import logo from "../Logo3.1.png";
+import { useGlobal } from "../state";
+import "antd/dist/antd.css";
+import Menu from "antd/es/menu";
+import Button from "antd/es/button";
 
+const firstTwoLetters = (str) =>
+  !str || str.length < 3 ? str : str.substring(0, 2);
 
-const firstTwoLetters = str => (!str || str.length < 3) ? str : str.substring(0, 2)
-
-const  Header = (props) => {
-  const [global,setGlobal, user] = useGlobal()
-  const authenticated = global.user
-    function renderNavItem(name){
-      const target = `${name.toLowerCase()}`
-      return (
-        <Menu.Item key={name} title={name}>
-          <Link to={`/${target}`} onClick={() =>{props.onClick(name)}}> {name}</Link>
-        </Menu.Item>
-      )
-    }
-
-    function authButton() {
-      const key = authenticated ? 'logout' : 'login'
-      const actionHolder = authenticated ?
-        <a onClick={() =>{setGlobal({ ...global, user: null, token:null }); localStorage.removeItem('serverApiToken') }}> LogOut </a>
-        :  <Link to='/login' onClick={() =>{props.onClick('login')}}> Login</Link>
-
-      return (
-        <Menu.Item key={key} title={key} style={{float: 'right'}}>
-          {actionHolder}
-        </Menu.Item>
-      )
-    }
-
-    function userBadge() {
-      const key = 'userBadge'
-      return (
-        <Menu.Item key={key} title={key} style={{float: 'right'}}>
-          {authenticated &&
-            <Button type="primary" shape="circle">
-              {firstTwoLetters(global.user.display_name)}
-            </Button>
-          }
-        </Menu.Item>
-      )
-    }
-
-    var pages = props.pages;
-    let navItems = [];
-    for (var i = 0; i < pages.length; i++) {
-      navItems.push(renderNavItem(pages[i]));
-    }
-
+const Header = (props) => {
+  const [global, setGlobal, user] = useGlobal();
+  const authenticated = global.user;
+  function renderNavItem(name) {
+    const target = `${name.toLowerCase()}`;
     return (
-      <Menu selectedKeys={[props.page]} mode="horizontal" className={'header'}>
-          <Menu.Item key='logo' title='logo'>
-            <Link to='home' onClick={() =>{props.onClick('Home')}}> <img src={logo} style={{height: '45px'}} /></Link>
-          </Menu.Item>
-          {navItems}
-          {authButton()}
-          {userBadge()}
-      </Menu>
-    )
+      <Menu.Item key={name.toLowerCase()} title={name}>
+        <Link to={`/${target}`}> {name}</Link>
+      </Menu.Item>
+    );
   }
 
-  export default Header;
+  function authButton() {
+    const key = authenticated ? "logout" : "login";
+    const actionHolder = authenticated ? (
+      <a
+        onClick={() => {
+          setGlobal({ ...global, user: null, token: null });
+          localStorage.removeItem("serverApiToken");
+        }}
+      >
+        {" "}
+        LogOut{" "}
+      </a>
+    ) : (
+      <Link to="/login"> Login</Link>
+    );
+
+    return (
+      <Menu.Item key={key} title={key} style={{ float: "right" }}>
+        {actionHolder}
+      </Menu.Item>
+    );
+  }
+
+  function userBadge() {
+    const key = "userBadge";
+    return (
+      <Menu.Item key={key} title={key} style={{ float: "right" }}>
+        {authenticated && (
+          <Button type="primary" shape="circle">
+            {firstTwoLetters(global.user.display_name)}
+          </Button>
+        )}
+      </Menu.Item>
+    );
+  }
+  debugger;
+  var pages = props.pages;
+  let navItems = [];
+  for (var i = 0; i < pages.length; i++) {
+    navItems.push(renderNavItem(pages[i]));
+  }
+
+  return (
+    <Menu
+      selectedKeys={props.location.pathname.substring(1)}
+      mode="horizontal"
+      className={"header"}
+    >
+      <Menu.Item key="logo" title="logo">
+        <Link to="home">
+          {" "}
+          <img src={logo} style={{ height: "45px" }} />
+        </Link>
+      </Menu.Item>
+      {navItems}
+      {authButton()}
+      {userBadge()}
+    </Menu>
+  );
+};
+
+export default withRouter(Header);
