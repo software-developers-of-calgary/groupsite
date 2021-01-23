6bfc60b427bbdf3516fea20ecc0be5be68bd8e83
Gergely Juhasz <gergelyjuhasz@Gergelys-MacBook-Pro.local>
2021-01-23 14:30:02 -0700

    auto login user if token saved in local store

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,62 +5,74 @@ import { useGlobal } from "../state";
 import "antd/dist/antd.css";
 import Menu from "antd/es/menu";
 import Button from "antd/es/button";
+import { fetchUser } from '../state/action';
 
 const firstTwoLetters = (str) =>
   !str || str.length < 3 ? str : str.substring(0, 2);
 
+function renderNavItem(name) {
+  const target = `${name.toLowerCase()}`;
+  return (
+    <Menu.Item key={name.toLowerCase()} title={name}>
+      <Link to={`/${target}`}> {name}</Link>
+    </Menu.Item>
+  );
+}
+
+function userBadge(authenticated, display_name) {
+  const key = "userBadge";
+  return (
+    <Menu.Item key={key} title={key} style={{ float: "right" }}>
+      {authenticated && (
+        <Button type="primary" shape="circle">
+          {firstTwoLetters(display_name)}
+        </Button>
+      )}
+    </Menu.Item>
+  );
+}
+
+function authButton(authenticated, onClick) {
+  const key = authenticated ? "logout" : "login";
+  const actionHolder = authenticated ? (
+    <a onClick={onClick}> LogOut </a>
+  ) : (
+    <Link to="/login"> Login</Link>
+  );
+
+  return (
+    <Menu.Item key={key} title={key} style={{ float: "right" }}>
+      {actionHolder}
+    </Menu.Item>
+  );
+}
+
+const autoLogin = (authenticated, global, setGlobal) => {
+  if (authenticated) {
+    return
+  }
+  const storedToken = localStorage.getItem('serverApiToken')
+  if (!storedToken || global.loadUser) {
+    return
+  }
+  fetchUser(storedToken, global, setGlobal)
+}
+
 const Header = (props) => {
-  const [global, setGlobal, user] = useGlobal();
+  const [global, setGlobal] = useGlobal();
   const authenticated = global.user;
-  function renderNavItem(name) {
-    const target = `${name.toLowerCase()}`;
-    return (
-      <Menu.Item key={name.toLowerCase()} title={name}>
-        <Link to={`/${target}`}> {name}</Link>
-      </Menu.Item>
-    );
-  }
 
-  function authButton() {
-    const key = authenticated ? "logout" : "login";
-    const actionHolder = authenticated ? (
-      <a
-        onClick={() => {
-          setGlobal({ ...global, user: null, token: null });
-          localStorage.removeItem("serverApiToken");
-        }}
-      >
-        {" "}
-        LogOut{" "}
-      </a>
-    ) : (
-      <Link to="/login"> Login</Link>
-    );
-
-    return (
-      <Menu.Item key={key} title={key} style={{ float: "right" }}>
-        {actionHolder}
-      </Menu.Item>
-    );
-  }
-
-  function userBadge() {
-    const key = "userBadge";
-    return (
-      <Menu.Item key={key} title={key} style={{ float: "right" }}>
-        {authenticated && (
-          <Button type="primary" shape="circle">
-            {firstTwoLetters(global.user.display_name)}
-          </Button>
-        )}
-      </Menu.Item>
-    );
-  }
+  autoLogin(authenticated, global, setGlobal)
   var pages = props.pages;
   let navItems = [];
   for (var i = 0; i < pages.length; i++) {
     navItems.push(renderNavItem(pages[i]));
   }
+
+  const onLogOut = () => {
+    setGlobal({ ...global, user: null, token: null });
+    localStorage.removeItem("serverApiToken");
+  };
 
   return (
     <Menu
@@ -75,8 +87,8 @@ const Header = (props) => {
         </Link>
       </Menu.Item>
       {navItems}
-      {authButton()}
-      {userBadge()}
+      {authButton(authenticated, onLogOut)}
+      {userBadge(authenticated, authenticated && global.user.display_name)}
     </Menu>
   );
 };
