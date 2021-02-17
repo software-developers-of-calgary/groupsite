fef34c39bd31bc6e60f84fd98b92ab5b351f9bc1
Gergely Juhasz <gergelyjuhasz@Gergelys-MacBook-Pro.local>
2021-02-16 21:37:38 -0700

    fetch event data from server

--- src/Components/Pages/EventPage.jsx
+++ src/Components/Pages/EventPage.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from 'axios';
+import { URL } from "../../config";
 import 'antd/dist/antd.css';
 import { withRouter } from "react-router-dom";
 import Description from '../Common/Description'
@@ -10,44 +12,77 @@ import ProjectList from "../Event/ProjectList";
 
 class ProjectPage extends React.Component {
 
-  state = {
-    isCollapsedSummary: true
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCollapsedSummary: true,
+      error: null,
+      isLoaded: false,
+      list: [],
+      keys : {},
+      date: '',
+      location: '',
+      name: '',
+      description: ''
+    };
+  }
+
+  componentDidMount() {
+    const eventId = this.props.match.params.eventId;
+    fetch(URL + `/events/${eventId}`)
+      .then(res => res.json())
+      .then(
+        ({date, description, location, name}) => {
+          this.setState({
+            isLoaded: true,
+            date,
+            description,
+            location,
+            name
+          });
+        },
+        (error) => {
+          console.log({error})
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   switchCollapse = () => {
     this.setState({isCollapsedSummary: !this.state.isCollapsedSummary})
   }
 
-  componentDidMount() {
-  }
-
   render() {
-
+    if (!this.state.isLoaded) {
+      return 'loading' // TODO create comp
+    }
+    if (this.state.error) {
+      return 'unexpected error' // TODO create comp
+    }
     return (
       <div>
         <br/>
-        <h2> Software Developers Of Calgary Hackhaton January</h2>
+        <h2> {this.state.name} </h2>
         <Row>
           <Col span={7}>
-            <TimeLoc date={"Jan. 30, 2021"} location={"https://meet.google.com/kgs-gjaa-uzx"}/>
+            <TimeLoc date={this.state.date} location={this.state.location}/>
           </Col>
           <Col span={1}>
           </Col>
           <Col span={15} >
-          <Description collapsed={this.state.isCollapsedSummary} onChange={this.switchCollapse} />
+          <Description data={this.state.description} collapsed={this.state.isCollapsedSummary} onChange={this.switchCollapse} />
           </Col>
           <Col span={1} />
         </Row>
           <div>
             <ProjectList />
           </div>
-
-
       </div>
-
     )
   }
-
 
 }
   export default withRouter(ProjectPage);
