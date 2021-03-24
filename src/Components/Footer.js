b0ee362b5e03a9b40d282396c8a0d46fb41e8888
misterhtmlcss <misterhtmlcss@gmail.com>
2021-03-23 22:26:22 -0600

    removed reduntent and overlaping tags and css

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,33 +8,16 @@ import { faEnvelopeSquare, faLaptop } from "@fortawesome/free-solid-svg-icons";
 export default function Footer() {
   return (
     <footer className="footer">
-      <Row>
-        <Col span={12}>
-          <a href="mailto:xyz">
-            <FontAwesomeIcon icon={faEnvelopeSquare} />
-            <span></span>mail to us
-          </a>
-        </Col>
-        <Col span={12}>
-          <a
-            target="blank"
-            href="https://discordapp.com/channels/515951809752465408/515952580103372810"
-          >
-            <FontAwesomeIcon icon={faLaptop} />
-            <span>discordapp.com</span>
-          </a>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={24}>
-          {" "}
-          © 2020 Copyright:
-          <a target="blank" href="#">
-            {" "}
-            this website{" "}
-          </a>
-        </Col>
-      </Row>
+      <a target="blank" href="https://discord.gg/t9r5VufW3d">
+        <FontAwesomeIcon icon={faLaptop} />
+        <span>Discord Community</span>
+      </a>
+      <div>
+        <span>© 2020 Copyright</span>
+        <a target="blank" href="https://sdc.fyi">
+          Software Developers of Calgary
+        </a>
+      </div>
     </footer>
   );
 }
