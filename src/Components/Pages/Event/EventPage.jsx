d7abd9fbf7dcd8cb71e93062d00508fd4c8e7580
Simon <29654835+absolutelysimon@users.noreply.github.com>
2021-04-24 21:09:57 +0000

    Project association added

--- src/Components/Pages/Event/EventPage.jsx
+++ src/Components/Pages/Event/EventPage.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import { URL } from "../../../config";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 import { withRouter } from "react-router-dom";
-import Description from '../../Common/Description'
-import Row from 'antd/es/row';
-import Col from 'antd/es/col';
+import Description from "../../Common/Description";
+import Row from "antd/es/row";
+import Col from "antd/es/col";
 import TimeLoc from "./TimeLoc";
 import UserList from "./UserList";
 import ProjectList from "../Project/ProjectList";
 import { Link } from "react-router-dom";
-import { Modal, Button } from 'antd';
+import { Modal, Button } from "antd";
+import "./EventPage.css";
 
 class EventPage extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -21,23 +21,24 @@ class EventPage extends React.Component {
       error: null,
       isLoaded: false,
       list: [],
-      keys : {},
-      date: '',
-      location: '',
-      name: '',
-      open: '',
+      keys: {},
+      date: "",
+      location: "",
+      name: "",
+      open: "",
       users: [],
-      description: '',
-      isAddProjectModalVisible: false
+      description: "",
+      projects: [],
+      isAddProjectModalVisible: false,
     };
   }
 
   componentDidMount() {
     const eventId = this.props.match.params.eventId;
     fetch(URL + `/events/${eventId}`)
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
-        ({date, description, location, name, open, users }) => {
+        ({ date, description, location, name, open, users }) => {
           this.setState({
             isLoaded: true,
             date,
@@ -45,94 +46,154 @@ class EventPage extends React.Component {
             location,
             users,
             name,
-            open
+            open,
           });
         },
         (error) => {
-          console.log({error})
+          console.log({ error });
           this.setState({
             isLoaded: true,
-            error
+            error,
           });
         }
-      )
+      );
   }
 
   registerUserForEvent(props) {
-    const token = localStorage.getItem('serverApiToken')
+    const token = localStorage.getItem("serverApiToken");
     const eventId = props.match.params.eventId;
-    axios.post(
-      `${URL}/events/${eventId}/users`,
-      '{}',
-      { headers: { Authorization: `Bearer ${token}` } }
-    )
-    .then(response => {
-      this.setState({users: response.data })
-    })
-    .catch(error => {
-      console.log(error);
-    })
+    axios
+      .post(`${URL}/events/${eventId}/users`, "{}", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        this.setState({ users: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   switchCollapse = () => {
-    this.setState({isCollapsedSummary: !this.state.isCollapsedSummary})
-  }
+    this.setState({ isCollapsedSummary: !this.state.isCollapsedSummary });
+  };
 
   showModal = () => {
-    this.setState({isAddProjectModalVisible: true})
+    this.setState({ isAddProjectModalVisible: true });
   };
 
   handleOk = () => {
-    console.log('handle ok', this)
-    this.setState({isAddProjectModalVisible: false})
+    console.log("handle ok", this);
+    this.setState({ isAddProjectModalVisible: false });
   };
 
   handleCancel = () => {
-    this.setState({isAddProjectModalVisible: false})
+    this.setState({ isAddProjectModalVisible: false });
+  };
+
+  associateProjectWithEvent = (projectId, eventId) => {
+    const token = localStorage.getItem("serverApiToken");
+    axios
+      .put(
+        URL + `/event/${eventId}/projects`,
+        {
+          projectId: projectId,
+        },
+        { headers: { Authorization: `Bearer ${token}` } }
+      )
+      .then((response) => {
+        this.setState({ projects: response.data });
+        this.handleCancel();
+      })
+      .catch((error) => {
+        console.log(error);
+        this.handleCancel();
+      });
   };
 
   render() {
     const eventId = this.props.match.params.eventId;
     if (!this.state.isLoaded) {
-      return 'loading' // TODO create comp
+      return "loading"; // TODO create comp
     }
     if (this.state.error) {
-      return 'unexpected error' // TODO create comp
+      return "unexpected error"; // TODO create comp
     }
     // const isAddUserButtonEnabled = this.state.date
     return (
       <div>
-        <br/>
+        <br />
         <h2> {this.state.name} </h2>
         <Row>
           <Col span={7}>
-            <TimeLoc date={this.state.date} location={this.state.location}/> <br/>
-            <UserList disabled={!this.state.open } users={this.state.users} register={() => this.registerUserForEvent(this.props)}/>
+            <TimeLoc date={this.state.date} location={this.state.location} />{" "}
+            <br />
+            <UserList
+              disabled={!this.state.open}
+              users={this.state.users}
+              register={() => this.registerUserForEvent(this.props)}
+            />
           </Col>
-          <Col span={1}>
-          </Col>
-          <Col span={15} >
-          <Description data={this.state.description} collapsed={this.state.isCollapsedSummary} onChange={this.switchCollapse} />
+          <Col span={1}></Col>
+          <Col span={15}>
+            <Description
+              data={this.state.description}
+              collapsed={this.state.isCollapsedSummary}
+              onChange={this.switchCollapse}
+            />
           </Col>
           <Col span={1} />
         </Row>
-          <div>
-            <h2> Projects </h2>
-            <Link to={`${eventId}/projects/new`}> Add new project</Link>
-            <button>Add existing project</button>
-            <Button type="primary" onClick={() => this.setState({isAddProjectModalVisible: true})}>
-        Open Modal
-      </Button>
-      <Modal title="Basic Modal" visible={this.state.isAddProjectModalVisible} onOk={() => this.handleOk()} onCancel={() => this.handleCancel()}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal>
-            <ProjectList onProjectLoaded={projects => this.setState({projects})} projects={this.state.projects} style={{margin: 'auto', marginTop: '10px', marginBorron: '10px' }}/>
-          </div>
+        <div>
+          <h2> Projects </h2>
+          <Link to={`${eventId}/projects/new`}> Add new project</Link>
+          <Button
+            type="primary"
+            onClick={() => this.setState({ isAddProjectModalVisible: true })}
+          >
+            Add Existing Project
+          </Button>
+          <Modal
+            title="Basic Modal"
+            visible={this.state.isAddProjectModalVisible}
+            footer={[]}
+            onCancel={this.handleCancel}
+          >
+            <div className="modal-container">
+              {this.state.projects?.map((project) => (
+                <div className="modal-project">
+                  <Link
+                    to={`/projects/${project.id}`}
+                    target="_blank"
+                    className="title"
+                  >
+                    {" "}
+                    {project.name}
+                  </Link>
+                  <Button
+                    className="modal-add-button"
+                    onClick={() =>
+                      this.associateProjectWithEvent(
+                        project.id,
+                        this.props.match.params.eventId
+                      )
+                    }
+                  >
+                    {" "}
+                    Import
+                  </Button>
+                </div>
+              ))}
+            </div>
+          </Modal>
+          <ProjectList
+            onProjectLoaded={(projects) => this.setState({ projects })}
+            projects={this.state.projects}
+            style={{ margin: "auto", marginTop: "10px", marginBorron: "10px" }}
+          />
+        </div>
       </div>
-    )
+    );
   }
-
 }
-  export default withRouter(EventPage);
+export default withRouter(EventPage);
