275540d753da0d1fddbe38a7d702021b9f2e8f68
Venetrius <gergelygjuhasz@gmail.com>
2021-04-24 11:11:45 -0600

    event-projects merge changes from outdated branch

---
+++ src/Components/Pages/CreateEventProject.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ProjectForm from './Project/NewProject'
+
+export default function CreateEventProject() {
+  console.log('rendering CreateEventProject')
+  return (
+    <div>
+      Hello world
+      <br/>
+        <ProjectForm
+          onSubmit={null}
+          projectsPage={null}
+          display={null}
+          resetSwitch={null}
+        />
+    </div>
+  )
+}
