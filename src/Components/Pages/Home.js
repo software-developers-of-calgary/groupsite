ca26158b00aea35ac964d26644fcdad34ad89d51
unknown <hosam_firas@hotmail.com>
2021-05-29 23:02:03 +0000

    Format p tags to be consistent

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -11,14 +11,12 @@ class Home extends React.Component {
         <div className="home-content">
           <h2> Welcome </h2>
           <p>
-            <i>
-              I was recently at a meetup in another city where a bunch of
-              current/new Software Developers would get together and work on
-              projects, or walk through tutorials together. It was useful. When
-              one person didn't know something, often someone else did. It was
-              just a helpful way to learn and grow as a software developer.{" "}
-              <br></br>Simon - January 26, 2019
-            </i>
+            "I was recently at a meetup in another city where a bunch of
+            current/new Software Developers would get together and work on
+            projects, or walk through tutorials together. It was useful. When
+            one person didn't know something, often someone else did. It was
+            just a helpful way to learn and grow as a software developer."
+            <span>~Simon, January 2019</span>
           </p>
           <p>
             I wanted to get this started as a regular thing in Calgary, where
@@ -27,12 +25,11 @@ class Home extends React.Component {
             new language or framework.
           </p>
           <p>
-            That was two years ago. We are now an organization of software
+            "That was two years ago. We are now an organization of software
             developers growing as quickly as we are able. But primarily we host
             monthly "mini-hackathons" where we spend a day coding in groups,
-            then towards the end of the day we share what we've learned!
-            <br />
-            Simon - January 25.2021
+            then towards the end of the day we share what we've learned!"
+            <span>~Simon, January 2021</span>
           </p>
         </div>
       </div>
