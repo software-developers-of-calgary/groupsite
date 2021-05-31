d0e741738292f62d8b77146769e88f848a479a34
Venetrius <gergelygjuhasz@gmail.com>
2021-05-30 19:21:51 -0600

    refactor auth

--- src/state/action.js
+++ src/state/action.js
@@ -8,11 +8,30 @@ export function fetchUser(token, global, setGlobal) {
     headers: { Authorization: `Bearer ${token}` },
   })
     .then((res) => {
+      console.log({profile: res.data.data})
       setGlobal({ ...global, user: res.data.data, loadUser: false });
     })
     .catch((error) => {
       setGlobal({ ...global, loadUser: false });
       localStorage.removeItem("serverApiToken");
       console.log("error", error);
+    });
+}
+
+export function session(token, global, setGlobal) {
+  console.log({ token });
+  if (!token) return;
+  setGlobal({ ...global, loadingSession: true });
+
+  Axios.post(`${URL}/auth/github/callback`, {
+    code: token,
+  })
+    .then((res) => {
+      localStorage.setItem("serverApiToken", res.data.token);
+      setGlobal({ ...global, loadingSession: false });
+    })
+    .catch((error) => {
+      console.log("error while authenticating with GitHub", error);
+      setGlobal({ ...global, loadingSession: false });
     });
 }
